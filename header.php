0221ea1b3b1480f2443fe8f9014fe483e1810835
Dominik Fischer <dom_fischer@web.de>
2016-05-25 20:26:02 +0200

    M1

--- header.php
+++ header.php
@@ -1,21 +1,22 @@
-<?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-//Json
-$tmp=file_get_contents('content/website.json');
-$j=json_decode($tmp, true) or die('damn it');
-//Header
-echo '<head><title>'.$j['settings']['title'].'</title><link rel="icon" href="../favicon.ico" type="image/x-icon"><link href="css/mainstyle.css" type="text/css" rel="stylesheet"><link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css"></head>';
-//Body
-echo '<body>';
-//Navibar
-echo '<div id="navibar"><div id="navcenter">';
-$c=0;
-while($j['navibar'][$c]['name']!=''){
-	echo '<div class="navibarbutton"><a href="'.$j['navibar'][$c]['url'].'">'.$j['navibar'][$c]['name'].'</a></div>';
-	$c++;
-}
-//Navibar Ende
-echo '</div></div>';
-//Pagebox
-echo '<div class="pagebox">';
-?>
+<head>
+	<title>Chaos Consulting e.V.</title>
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<link href="css/main.css" type="text/css" rel="stylesheet">
+	<link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	<div id="navibar">
+		<div id="navcenter">
+		<?php
+			$mydirname = 'pages/';
+			$dir = new DirectoryIterator($mydirname);
+			foreach ($dir as $fileinfo) {
+				if ($fileinfo->isDir() and ! $fileinfo->isDot() ) {
+					echo '<div class="navibarbutton"><a href="page.php?p='.$fileinfo.'">'.strtoupper($fileinfo).'</a></div>';
+				}
+			}
+			//Navibar Ende
+		?>
+		</div>
+	</div>
+<div class="pagebox">
---
+++ page.php
@@ -0,0 +1,7 @@
+<?php
+	$page = $_GET["p"];
+	include("header.php");
+?>
+	<div class="contentbox">
+		<h2><?php echo strtoupper($page); ?></h2>
+	</div>
