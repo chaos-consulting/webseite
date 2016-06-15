53055d2f4c7cb93d45c20baac202c231a7620c30
Jan-Alexander Mock <janalexander.mock@gmail.com>
2016-06-15 21:13:58 +0200

    Loading of projects works now

--- projects.php
+++ projects.php
@@ -5,10 +5,13 @@ PhpXmlRpc\Autoloader::register();
 
 echo "Dies ist die Projekt-Seite";
 
-$req = new PhpXmlRpc\Request('dokuwiki.getPagelist', array("Projekte"));
+$namespace = new PhpXmlRpc\Value("projekte");
+
+$req = new PhpXmlRpc\Request('dokuwiki.getPagelist', array($namespace));
 $user = "apiuser";
 $password = "yhPxMd9DdwqY6W";
-$client = new PhpXmlRpc\Client("https://cloud.chaos-consulting.de/dokuwiki/lib/exe/xmlrpc.php");
+$url = "https://cloud.chaos-consulting.de/dokuwiki/lib/exe/xmlrpc.php";
+$client = new PhpXmlRpc\Client($url);
 $client->setSSLVerifyPeer(false);
 $client->setSSLVerifyHost(0);
 $client->setCredentials($user, $password);
@@ -16,7 +19,18 @@ $client->setCredentials($user, $password);
 $resp = $client->send($req);
 
 if(!$resp->faultCode()) {
-    print "No error";
+    $encoder = new PhpXmlRpc\Encoder();
+
+    foreach($encoder->decode($resp->value()) as $projekt) {
+        $projekt_id = $projekt["id"];
+        if(substr_count($projekt_id, ":")<2)
+            continue;
+
+        echo "<div>";
+        echo "Projekt: " . $projekt_id . " ";
+        print_r($projekt);
+        echo "</div>";
+    }
 } else {
     print "\nAn error occured: ";
     print "Code: " . htmlspecialchars($resp->faultCode()) . " ";
